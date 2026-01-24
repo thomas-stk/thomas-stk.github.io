16d3f58568587861163fcfeb35942f493cf7f8bb
Thomas <thomas.stk.7@gmail.com>
2026-01-24 15:22:02 +0000

    Remove about section for now

--- src/App.js
+++ src/App.js
@@ -133,9 +133,7 @@ function App() {
           </div>
 
           {/* About Section */}
-          {/* <div className='hidden'>
-            <h2 className='text-xl text-'>About</h2>
-          </div> */}
+          
 
         </div>
       </div>
